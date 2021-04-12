19518d8a16f7443df5da25c2c82ce6e9b9298dc5
Bartek <bartekgrzesiuk@gmail.com>
2021-04-12 20:51:44 +0200

    client test added, more compleks Select query tested

---
+++ src/test/java/com/pl/bg/javamasproject/demo/models/ClientTest.java
@@ -0,0 +1,35 @@
+package com.pl.bg.javamasproject.demo.models;
+
+import com.pl.bg.javamasproject.demo.tools.Looper;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @Test
+    void fields() {
+
+        List<String> listColumns = Arrays.asList("int","String","long","double");
+        List<String> fieldsList = new ArrayList<>();
+        Field[] tab =new Patient().getClass().getDeclaredFields();
+        Looper.forLoop(0,tab.length, i -> {
+        for(int j =0;j<listColumns.size();j++) {
+
+            if (!tab[i].getType().getSimpleName().equals(listColumns.get(j))) {
+
+            } else {
+                fieldsList.add(tab[i].getName());
+            }
+        }
+
+        });
+        System.out.println(fieldsList);
+    }
+
+}
