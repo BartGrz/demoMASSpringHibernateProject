7449e62f992778a29dbd533dfb7c8fa00f65b523
Bartek <bartekgrzesiuk@gmail.com>
2021-04-14 00:25:33 +0200

    GUI for testing created

--- src/test/java/com/pl/bg/javamasproject/demo/controllers/PatientControllerTest.java
+++ src/test/java/com/pl/bg/javamasproject/demo/controllers/PatientControllerTest.java
@@ -1,16 +1,16 @@
 package com.pl.bg.javamasproject.demo.controllers;
 
 
-import com.pl.bg.javamasproject.demo.SQL.CompleksSelectQuery;
+
 import com.pl.bg.javamasproject.demo.SQL.InsertQueryBuilder;
 import com.pl.bg.javamasproject.demo.SQL.Repository;
-import com.pl.bg.javamasproject.demo.models.Client;
+
 import com.pl.bg.javamasproject.demo.models.Patient;
 
 import com.pl.bg.javamasproject.demo.tools.Looper;
 import org.junit.jupiter.api.Test;
 import java.util.List;
-import java.util.Locale;
+
 
 class PatientControllerTest {
 
@@ -30,36 +30,20 @@ class PatientControllerTest {
     @Test
     void showRecords() {
 
-        List<Object[]> patientList = new CompleksSelectQuery.Builder<Patient>()
-                .from(new Patient())
-                .end()
-                .buildSelectQuery();
-
-        Looper.forLoop(0,patientList.size(),i -> {
-            for(int j = 0;j<patientList.get(0).length;j++) {
-
-                System.out.println(patientList.get(i)[j]+" ");
-            }
-
-        });
-
 
     }
 
     @Test
     void addRecord() {
 
-        new InsertQueryBuilder.Builder<Client>()
-                .insertInto(Client.class)
-                .fields(new Client().fields())
-                .value("Jan")
-                .end()
-                .generateAndExecuteSQL();
 
         new InsertQueryBuilder.Builder<Patient>()
                 .insertInto(Patient.class)
                 .fields(new Patient().fields())
-                .value("testest").value(1).value(1)
-                .end().generateAndExecuteSQL();
+                .value("kropka")
+                .value(3)
+                .value(3)
+                .end()
+                .generateAndExecuteSQL();
     }
 }
