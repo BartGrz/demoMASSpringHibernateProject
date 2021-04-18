91628cb786bd3fe13947c2ed8770ce68483ad6b4
Bartek <bartekgrzesiuk@gmail.com>
2021-04-19 00:05:49 +0200

    spring boot starter annot deleted

--- src/main/java/com/pl/bg/javamasproject/demo/DemoApplication.java
+++ src/main/java/com/pl/bg/javamasproject/demo/DemoApplication.java
@@ -1,27 +1,20 @@
 package com.pl.bg.javamasproject.demo;
 
+import com.pl.bg.javamasproject.demo.Beans.AppConfiguration;
 import com.pl.bg.javamasproject.demo.controllers.PatientController;
-import com.pl.bg.javamasproject.demo.models.Patient;
-import com.pl.bg.javamasproject.demo.tools.FXML_tools.TableViewCreator;
+import com.pl.bg.javamasproject.demo.models.PatientRepository;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 
-import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
 
-@SpringBootApplication
 public class DemoApplication extends Application  {
-
-
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(DemoApplication.class, args);
